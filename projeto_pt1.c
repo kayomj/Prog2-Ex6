046e8a09267886d33db1ab6772a563188d7d8719
solonmota <42355251+solonmota@users.noreply.github.com>
2018-10-10 15:42:24 -0300

    problemas com funcao salvar_arq

--- projeto_pt1.c
+++ projeto_pt1.c
@@ -2,13 +2,13 @@
 #include<stdlib.h>
 #include<time.h>
 
-int vet[150];
 
 int gera(){
 	return rand()%199;
 }
 
-void rb(int n)
+
+void rb(int vet[], int n)
 {
     if(n == 0) 
         return;
@@ -22,26 +22,49 @@ void rb(int n)
         vet[i] = aux;
     	}
 	}
-    rb(n - 1);
+    rb(vet, n - 1);
 }
 
-void print(){
+
+void salvar_arq(int vet[150]){
+	FILE *arq;
+	arq=fopen("texto.txt","wb");
+	
+	if(arq==NULL){
+		printf("problemas com abertura do arquivo");
+		exit(1);
+	}
+	for(int i=0;i<150;i++){
+		if(fwrite(&vet[i],1,sizeof(vet[i]),arq)!=1){
+			printf("erro na escrita do arquivo");
+			exit(1);
+		}
+	
+	}
+	fclose(arq);
+}
+
+
+void print(int vet[]){
 	for(int i=0;i<150;i++){
 		printf("%3.d\n",vet[i]);
 	}
 }
+
+
 int main(){
+	int vet[150];
 	int cont=0; 
 	srand(time(NULL));
 	for(int i=0;i<150;i++){
 		vet[i]=gera();
 		
 	}
-	print();
+	print(vet);
 	printf("%d\n", cont);
-	rb(150);
+	rb(vet,150);
 	printf("\n---------------------pos ordenacao---------------------\n");
-	print();
-
+	print(vet);
+	salvar_arq(vet);
 	return 0;
 }
